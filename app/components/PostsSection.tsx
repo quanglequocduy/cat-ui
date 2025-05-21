3becd80338f21ab881ccc3f78b1ca8a03739b84a
quanglequocduy <quangle@hipagesgroup.com.au>
2025-05-21 13:31:50 +0700

    chore: update css

--- app/components/PostsSection.tsx
+++ app/components/PostsSection.tsx
@@ -7,11 +7,11 @@ interface PostsSectionProps {
 export default function PostsSection({ posts }: PostsSectionProps) {
   return (
     <section id="activities">
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col">
         {posts.map((item) => (
           <div
             key={item.id}
-            className="flex flex-col md:flex-row border rounded-lg overflow-hidden shadow-md w-full gap-5"
+            className="flex flex-col md:flex-row border rounded-lg overflow-hidden shadow-md w-full gap-5 p-2"
           >
             {/* Hình ảnh bên trái */}
               <img
