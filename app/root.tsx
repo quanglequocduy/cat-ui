5655bbf00a8ecd3fd4ce3cd60f304a6574f95333
quanglequocduy <quangle@hipagesgroup.com.au>
2025-05-21 13:09:23 +0700

    chore: update root

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,7 @@
 import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
 import Header from "~/components/Header";
 import Footer from "~/components/Footer";
-import { useState, useEffect } from "react";
-import { Post, Category } from "./types";
+import tailwindStyles from "~/tailwind.css";
 
 export function links() {
   return [
@@ -10,7 +9,7 @@ export function links() {
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap",
     },
-    { rel: "stylesheet", href: "/app/tailwind.css" }, // Import CSS qua links()
+    { rel: "stylesheet", href: tailwindStyles },
   ];
 }
 
